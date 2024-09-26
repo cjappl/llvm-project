70f3602b5d923aa8b354bac9948e8b4864d8d3ce
Chris Apple <cja-private@pm.me>
2024-09-26 07:04:10 -0700

    DEMO - symbolize stack

--- compiler-rt/lib/rtsan/rtsan.h
+++ compiler-rt/lib/rtsan/rtsan.h
@@ -45,6 +45,5 @@ SANITIZER_INTERFACE_ATTRIBUTE void __rtsan_enable();
 SANITIZER_INTERFACE_ATTRIBUTE void
 __rtsan_notify_intercepted_call(const char *intercepted_function_name);
 
-SANITIZER_INTERFACE_ATTRIBUTE void
-__rtsan_notify_blocking_call(const char *blocking_function_name);
+SANITIZER_INTERFACE_ATTRIBUTE void __rtsan_notify_blocking_call();
 } // extern "C"
